a88b133912e44d3c5b294aa2a1a8798dfc6ac49b
DEVENDRA-5470 <your_email@example.com>
2026-02-20 17:55:54 +0530

    updated

--- Flask-100/myapis/app.py
+++ Flask-100/myapis/app.py
@@ -23,4 +23,20 @@ def accounts():
 def delete_account():
     return "I am delete funtion and call when you clicked on delete"
 
+@app.route("/register-now")
+def register():
+    return render_template("register.html")
+
+@app.route("/create-account",methods=["POST"])
+def create_account():
+    if request.method=="POST":
+        cus_ac_no=request.form.get("cus_ac_no")
+        cus_name=request.form.get("cus_name")
+        cus_mob=request.form.get("cus_mob")
+        cus_add=request.form.get("cus_add")
+
+        
+    else:
+        return "I AM GET"
+
 app.run(debug=True)
